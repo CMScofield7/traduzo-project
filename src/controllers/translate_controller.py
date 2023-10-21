185dae115c7c2fff203bdb425343306da70669eb
Laion Brito <llbchagas@gmail.com>
2023-10-21 07:19:12 -0300

    Requisito 4 e 5 Concluídos! - Endpoint Tradutor, renderizando variáveis do Backend (GET) e Tradução de Texto (POST)

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -1,7 +1,7 @@
-from flask import Blueprint, render_template
-# from deep_translator import GoogleTranslator
-# from models.language_model import LanguageModel
-# from models.history_model import HistoryModel
+from flask import Blueprint, render_template, request
+from deep_translator import GoogleTranslator
+from models.language_model import LanguageModel
+from models.history_model import HistoryModel
 
 
 translate_controller = Blueprint("translate_controller", __name__)
@@ -10,7 +10,33 @@ translate_controller = Blueprint("translate_controller", __name__)
 # Reqs. 4 e 5
 @translate_controller.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    languages = LanguageModel.find()
+    if request.method == "GET":
+        return render_template(
+            "index.html",
+            languages=[language.to_dict() for language in languages],
+            text_to_translate="O que deseja traduzir?",
+            translate_from="pt",
+            translate_to="en",
+            translated="What do you want to translate?",
+        )
+    elif request.method == "POST":
+        text_to_translate = request.form["text-to-translate"]
+        translate_from = request.form["translate-from"]
+        translate_to = request.form["translate-to"]
+
+        translated = GoogleTranslator(
+            source=translate_from, target=translate_to
+        ).translate(text_to_translate)
+
+        return render_template(
+            "index.html",
+            languages=[language.to_dict() for language in languages],
+            text_to_translate=text_to_translate,
+            translate_from=translate_from,
+            translate_to=translate_to,
+            translated=translated,
+        )
 
 
 # Req. 6
