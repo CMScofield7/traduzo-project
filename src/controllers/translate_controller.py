002a3fb9b4c0936326eeebb4bd49928187b509fc
Laion Brito <llbchagas@gmail.com>
2023-10-21 07:32:24 -0300

    Requisito 6 Concluído! - Tradução Reversa (POST)

--- src/controllers/translate_controller.py
+++ src/controllers/translate_controller.py
@@ -43,4 +43,20 @@ def index():
 # Req. 6
 @translate_controller.route("/reverse", methods=["POST"])
 def reverse():
-    raise NotImplementedError
+    language = LanguageModel.find()
+    text_to_translate = request.form["text-to-translate"]
+    translate_from = request.form["translate-from"]
+    translate_to = request.form["translate-to"]
+
+    translated = GoogleTranslator(
+        source=translate_from, target=translate_to
+    ).translate(text_to_translate)
+
+    return render_template(
+        "index.html",
+        languages=[language.to_dict() for language in language],
+        text_to_translate=translated,
+        translate_from=translate_to,
+        translate_to=translate_from,
+        translated=text_to_translate,
+    )
