70bd2938a81ef07a2b770914b99d31e33ff96258
Laion Brito <llbchagas@gmail.com>
2023-10-21 07:42:13 -0300

    Requisito 7 Concluído! - TESTE: Histórico de Traduções

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,12 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+from src.models.history_model import HistoryModel
 
 
 # Req. 7
 def test_request_history():
-    raise NotImplementedError
+    history_json = HistoryModel.list_as_json()
+    history_data = json.loads(history_json)
+
+    assert "Hello, I like videogame" in history_data[0]["text_to_translate"]
+    assert "en" in history_data[0]["translate_from"]
+    assert "pt" in history_data[0]["translate_to"]
